05eeeeac8d8cef5b75baf7b7ba7a8c864fabaed7
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2025-07-10 23:48:10 +0900

    Core Dataスタックの追加とAppへの組み込み

--- BinBuddy/BinBuddyApp.swift
+++ BinBuddy/BinBuddyApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import CoreData
 
 @main
 struct BinBuddyApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
         }
     }
 }
